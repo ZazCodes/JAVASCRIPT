6a6c273b90928c0c8a5eb09b4a032c292718a863
ZazCodes <zazcodes@gmail.com>
2023-12-01 18:24:13 +0500

    moved a project

--- js.practice/header-with-js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let menu = document.querySelector(".menu");
-    let section = document.querySelector(".sec");
-
-    menu.addEventListener("click", function () {
-        section.classList.toggle("nav");
-    });
-});
